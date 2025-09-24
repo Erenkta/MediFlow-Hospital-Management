657245c56cd567aad1db1b102e7af7624a392441
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2025-09-24 21:10:19 +0300

    Initialize Project
    Mediflow project has initialized from
    🔗 https://start.spring.io/

---
+++ src/test/java/com/hospital/mediflow/MediflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.mediflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
