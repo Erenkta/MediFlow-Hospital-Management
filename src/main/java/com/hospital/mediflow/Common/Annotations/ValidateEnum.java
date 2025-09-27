ba46c10ef890e831330a40e983d9aadfe71ddad2
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2025-09-27 18:05:18 +0300

    Enums and Validations Created
    I have created 2 enums for doctor domain class. And I have also added a validation for ENUM type properties

---
+++ src/main/java/com/hospital/mediflow/Common/Annotations/ValidateEnum.java
@@ -0,0 +1,20 @@
+package com.hospital.mediflow.Common.Annotations;
+
+import com.hospital.mediflow.Common.Helpers.EnumValidatorImpl;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = EnumValidatorImpl.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidateEnum {
+    String message() default "Invalid enum value";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<? extends Enum<?>> enumClass();
+}
