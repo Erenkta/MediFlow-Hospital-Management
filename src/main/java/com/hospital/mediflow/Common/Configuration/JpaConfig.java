ab52c2f43c0dcc3e5f45fe51cc70b40c44e99de1
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2025-09-27 18:09:22 +0300

    Created Base Entity And Doctor
    I have created base entity for my application and as well as a Doctor entity  for the  'doctors ' table. I have used @PrePersist to generate the  doctor code using the title and specialty of the doctor entity. @PostPersist is used for logging purposes.
    
    And I have also used Auditing feature.

---
+++ src/main/java/com/hospital/mediflow/Common/Configuration/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.hospital.mediflow.Common.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
