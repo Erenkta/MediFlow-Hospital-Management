d57aa18622efddf7ce45fbe8d1ab6931892cee9e
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2025-09-28 19:07:59 +0300

    Update business service and data service
    I have added some methods to data service and business service. I have validate the id parameters with @NonNull and @Validated annotations. And I'll handle validation exceptions with global exception handler.

--- src/main/java/com/hospital/mediflow/Doctor/Services/Abstracts/DoctorService.java
+++ src/main/java/com/hospital/mediflow/Doctor/Services/Abstracts/DoctorService.java
@@ -1,12 +1,20 @@
 package com.hospital.mediflow.Doctor.Services.Abstracts;
 
+import com.hospital.mediflow.Doctor.Domain.Dtos.DoctorFilterDto;
 import com.hospital.mediflow.Doctor.Domain.Dtos.DoctorRequestDto;
 import com.hospital.mediflow.Doctor.Domain.Dtos.DoctorResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DoctorService {
     DoctorResponseDto saveDoctor(DoctorRequestDto requestDto);
-    Optional<DoctorResponseDto> findDoctorById(Long id);
-    Optional<DoctorResponseDto> findDoctorByDoctorCode(Long doctorCode);
+    DoctorResponseDto updateDoctor(Long id,DoctorRequestDto requestDto);
+    DoctorResponseDto findDoctorById(Long id);
+    DoctorResponseDto findDoctorByDoctorCode(Long doctorCode);
+    List<DoctorResponseDto> findDoctors(DoctorFilterDto filter);
+    Page<DoctorResponseDto> findDoctors(Pageable pageable, DoctorFilterDto filter);
+
 }
