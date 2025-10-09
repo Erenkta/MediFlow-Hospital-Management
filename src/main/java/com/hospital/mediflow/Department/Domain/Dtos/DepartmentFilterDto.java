e6e5d07ff7729ac88922e063eb1e91d5dc13ec74
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2025-10-09 21:19:40 +0300

    Write Integration tests of Department
    I have wrote some Integration tests of Department modules

--- src/main/java/com/hospital/mediflow/Department/Domain/Dtos/DepartmentFilterDto.java
+++ src/main/java/com/hospital/mediflow/Department/Domain/Dtos/DepartmentFilterDto.java
@@ -1,7 +1,10 @@
 package com.hospital.mediflow.Department.Domain.Dtos;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder(toBuilder = true)
 public record DepartmentFilterDto(
         String name,
         String description,
---
+++ src/test/java/com/hospital/mediflow/Department/Integration/DepartmentIntegrationTest.java
@@ -0,0 +1,404 @@
+package com.hospital.mediflow.Department.Integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hospital.mediflow.Common.Exceptions.ErrorCode;
+import com.hospital.mediflow.Department.Domain.Dtos.DepartmentFilterDto;
+import com.hospital.mediflow.Department.Domain.Dtos.DepartmentRequestDto;
+import com.hospital.mediflow.Department.Domain.Entity.Department;
+import com.hospital.mediflow.Department.Repository.DepartmentRepository;
+import com.hospital.mediflow.Doctor.Domain.Dtos.DoctorRequestDto;
+import com.hospital.mediflow.Doctor.Enums.TitleEnum;
+import com.hospital.mediflow.Specialty.Domain.Entity.Specialty;
+import com.hospital.mediflow.Specialty.Repositories.SpecialtyRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+public class DepartmentIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private DepartmentRepository repository;
+
+    @Autowired
+    private SpecialtyRepository specialtyRepository;
+    @Autowired
+    private ObjectMapper mapper;
+
+    private final String API_URI ="/api/v1/departments";
+
+    // Create Department Tests
+    @Test
+    void should_save_department() throws Exception {
+        DepartmentRequestDto requestDto = DepartmentRequestDto.builder()
+                .name("Test Department")
+                .description("Mockito Test Department")
+                .specialties(List.of("000","001"))
+                .build();
+
+        String content = mapper.writeValueAsString(requestDto);
+
+        mockMvc.perform(post(API_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Test Department"))
+                .andExpect(jsonPath("$.description").value("Mockito Test Department"));
+    }
+    @Test
+    void should_save_department_when_specialties_are_empty() throws Exception {
+        DepartmentRequestDto requestDto = DepartmentRequestDto.builder()
+                .name("Test Department")
+                .description("Mockito Test Department")
+                .specialties(List.of())
+                .build();
+
+        String content = mapper.writeValueAsString(requestDto);
+
+        mockMvc.perform(post(API_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Test Department"))
+                .andExpect(jsonPath("$.description").value("Mockito Test Department"));
+    }
+    @Test
+    void should_create_department_throw_exception_when_specialty_value_size_is_invalid() throws Exception {
+        DepartmentRequestDto requestDto = DepartmentRequestDto.builder()
+                .name("Test Department")
+                .description("Mockito Test Department")
+                .specialties(List.of("99933322"))
+                .build();
+
+        String content = mapper.writeValueAsString(requestDto);
+
+        mockMvc.perform(post(API_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().is(Integer.parseInt(ErrorCode.METHOD_ARGUMENT_NOT_VALID.getStatusCode())))
+                .andExpect(jsonPath("$.message").value("Argument validation has failed."))
+                .andExpect(jsonPath("$.fieldErrorList.length()").value(1))
+                .andExpect(jsonPath("$.fieldErrorList.[0].rejectedValue").value("99933322"))
+                .andExpect(jsonPath("$.errorCode").value("METHOD_ARGUMENT_NOT_VALID"));
+    }
+    @Test
+    void should_create_department_throw_exception_when_specialties_are_invalid() throws Exception {
+        DepartmentRequestDto requestDto = DepartmentRequestDto.builder()
+                .name("Test Department")
+                .description("Mockito Test Department")
+                .specialties(List.of("zzz","007"))
+                .build();
+
+        String content = mapper.writeValueAsString(requestDto);
+
+        mockMvc.perform(post(API_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().is(Integer.parseInt(ErrorCode.RECORD_NOT_FOUND.getStatusCode())))
+                .andExpect(jsonPath("$.message").value("Some of the given specialties could not be found. Please check the specialty codes and try again. Check List: [zzz]"))
+                .andExpect(jsonPath("$.errorCode").value("RECORD_NOT_FOUND"));
+    }
+    @Test
+    void should_create_department_throw_exception_when_department_name_duplicated() throws Exception {
+        Department request = Department.builder()
+                .name("Test Department")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test").code("999").build()))
+                .build();
+
+        DepartmentRequestDto duplicatedNameDto = DepartmentRequestDto.builder()
+                .name("Test Department")
+                .description("Mockito Test Department")
+                .specialties(List.of("zzz","007"))
+                .build();
+
+        repository.save(request);
+
+        String content = mapper.writeValueAsString(duplicatedNameDto);
+
+        mockMvc.perform(post(API_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().is(Integer.parseInt(ErrorCode.RECORD_ALREADY_EXISTS.getStatusCode())))
+                .andExpect(jsonPath("$.message").value("Department with name Test Department already exists"))
+                .andExpect(jsonPath("$.errorCode").value("RECORD_ALREADY_EXISTS"));
+    }
+    //
+
+    // Get Department Tests ( List and Page )
+    @Test
+    void should_get_departments_return_all_departments_list() throws Exception {
+        Department requestNo1 = Department.builder()
+                .name("Test Department NO1")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO1").code("999").build()))
+                .build();
+
+        Department requestNo2 = Department.builder()
+                .name("Test Department NO2")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO2").code("998").build()))
+                .build();
+
+        repository.saveAll(List.of(requestNo1, requestNo2));
+
+        mockMvc.perform(get(API_URI))
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("length()").value(2));
+    }
+
+    @Test
+    void should_get_departments_return_all_departments_list_name_filtered() throws Exception {
+        Department requestNo1 = Department.builder()
+                .name("Test Department NO1")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO1").code("999").build()))
+                .build();
+
+        Department requestNo2 = Department.builder()
+                .name("Test Department NO2")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO2").code("998").build()))
+                .build();
+
+        DepartmentFilterDto filter = DepartmentFilterDto.builder().name("Test Department NO1").build();
+
+        repository.saveAll(List.of(requestNo1, requestNo2));
+
+        mockMvc.perform(get(API_URI)
+                        .queryParam("name", filter.name()))
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$[0].specialties[0].name").value("Test NO1"))
+                .andExpect(jsonPath("length()").value(1));
+    }
+    @Test
+    void should_get_departments_return_all_departments_list_description_filtered() throws Exception {
+        Department requestNo1 = Department.builder()
+                .name("Test Department NO1")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO1").code("999").build()))
+                .build();
+
+        Department requestNo2 = Department.builder()
+                .name("Test Department NO2")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO2").code("998").build()))
+                .build();
+
+        repository.saveAll(List.of(requestNo1, requestNo2));
+
+        mockMvc.perform(get(API_URI)
+                        .queryParam("description", "Test"))
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("length()").value(2));
+    }
+    @Test
+    void should_get_departments_return_all_departments_list_specialty_filtered() throws Exception {
+        Specialty specialty1 = Specialty.builder().name("Test NO1").code("999").build();
+        Specialty specialty2 = Specialty.builder().name("Test NO2").code("998").build();
+
+        specialtyRepository.saveAll(List.of(specialty1,specialty2));
+        Department requestNo1 = Department.builder()
+                .name("Test Department NO1")
+                .description("Mockito Test Department")
+                .specialties(List.of(specialty1))
+                .build();
+
+        Department requestNo2 = Department.builder()
+                .name("Test Department NO2")
+                .description("Mockito Test Department")
+                .specialties(List.of(specialty2))
+                .build();
+
+        requestNo1 = repository.save(requestNo1);
+        requestNo2 = repository.save(requestNo2);
+        specialty1.setDepartment(requestNo1);
+        specialty2.setDepartment(requestNo2);
+        specialtyRepository.saveAll(List.of(specialty1,specialty2));
+
+        mockMvc.perform(get(API_URI)
+                        .queryParam("specialties","999"))
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$[0].specialties[0].name").value("Test NO1"))
+                .andExpect(jsonPath("length()").value(1));
+    }
+    @Test
+    void should_get_departments_return_empty_list_when_filter_is_not_match() throws Exception {
+        Department requestNo1 = Department.builder()
+                .name("Test Department NO1")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO1").code("999").build()))
+                .build();
+
+        Department requestNo2 = Department.builder()
+                .name("Test Department NO2")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO2").code("998").build()))
+                .build();
+
+        repository.saveAll(List.of(requestNo1, requestNo2));
+
+        mockMvc.perform(get(API_URI)
+                        .queryParam("description", "Wont be match"))
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("length()").value(0));
+    }
+
+    @Test
+    void should_get_departments_return_all_departments_page_name_filtered() throws Exception {
+        Department requestNo1 = Department.builder()
+                .name("Test Department NO1")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO1").code("999").build()))
+                .build();
+
+        Department requestNo2 = Department.builder()
+                .name("Test Department NO2")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO2").code("998").build()))
+                .build();
+
+        DepartmentFilterDto filter = DepartmentFilterDto.builder().name("Test Department NO1").build();
+
+        repository.saveAll(List.of(requestNo1, requestNo2));
+
+        mockMvc.perform(get(API_URI)
+                        .queryParam("name", filter.name())
+                        .param("size","2")
+                        .param("page","0"))
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.content.[0].specialties[0].name").value("Test NO1"))
+                .andExpect(jsonPath("$.size").value(2));
+    }
+    @Test
+    void should_get_departments_return_all_departments_page_description_filtered() throws Exception {
+        Department requestNo1 = Department.builder()
+                .name("Test Department NO1")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO1").code("999").build()))
+                .build();
+
+        Department requestNo2 = Department.builder()
+                .name("Test Department NO2")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO2").code("998").build()))
+                .build();
+
+        repository.saveAll(List.of(requestNo1, requestNo2));
+
+        mockMvc.perform(get(API_URI)
+                        .param("size","2")
+                        .param("page","0")
+                        .queryParam("description", "Test"))
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.content.length()").value(2))
+                .andExpect(jsonPath("$.size").value(2));
+    }
+    @Test
+    void should_get_departments_return_all_departments_page_specialty_filtered() throws Exception {
+        Specialty specialty1 = Specialty.builder().name("Test NO1").code("999").build();
+        Specialty specialty2 = Specialty.builder().name("Test NO2").code("998").build();
+
+        specialtyRepository.saveAll(List.of(specialty1,specialty2));
+        Department requestNo1 = Department.builder()
+                .name("Test Department NO1")
+                .description("Mockito Test Department")
+                .specialties(List.of(specialty1))
+                .build();
+
+        Department requestNo2 = Department.builder()
+                .name("Test Department NO2")
+                .description("Mockito Test Department")
+                .specialties(List.of(specialty2))
+                .build();
+
+        requestNo1 = repository.save(requestNo1);
+        requestNo2 = repository.save(requestNo2);
+        specialty1.setDepartment(requestNo1);
+        specialty2.setDepartment(requestNo2);
+        specialtyRepository.saveAll(List.of(specialty1,specialty2));
+
+        mockMvc.perform(get(API_URI)
+                        .param("size","2")
+                        .param("page","0")
+                        .queryParam("specialties","999"))
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.content.[0].specialties[0].name").value("Test NO1"))
+                .andExpect(jsonPath("$.content.length()").value(1))
+                .andExpect(jsonPath("$.size").value(2));
+    }
+    @Test
+    void should_get_departments_return_empty_page_when_filter_is_not_match() throws Exception {
+        Department requestNo1 = Department.builder()
+                .name("Test Department NO1")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO1").code("999").build()))
+                .build();
+
+        Department requestNo2 = Department.builder()
+                .name("Test Department NO2")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO2").code("998").build()))
+                .build();
+
+        repository.saveAll(List.of(requestNo1, requestNo2));
+
+        mockMvc.perform(get(API_URI)
+                        .param("size","2")
+                        .param("page","0")
+                        .queryParam("description", "Wont be match"))
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.content.length()").value(0));
+    }
+    //
+
+    // Get Department By ID Tests
+
+    @Test
+    void should_get_department_by_id_is_successful() throws Exception {
+        Department department = Department.builder()
+                .name("Test Department NO1")
+                .description("Mockito Test Department")
+                .specialties(List.of(Specialty.builder().name("Test NO1").code("999").build()))
+                .build();
+
+
+        Long departmentId =  repository.save(department).getId();
+
+        mockMvc.perform(get(API_URI+"/"+departmentId))
+                .andExpect(status().is(HttpStatus.FOUND.value()))
+                .andExpect(jsonPath("$.id").value(departmentId))
+                .andExpect(jsonPath("$.specialties.length()").value(department.getSpecialties().size()));
+    }
+    @Test
+    void should_get_department_by_id_throw_exception_when_not_found() throws Exception {
+        mockMvc.perform(get(API_URI+"/"+999L))
+                .andExpect(status().is(Integer.parseInt(ErrorCode.RECORD_NOT_FOUND.getStatusCode())))
+                .andExpect(jsonPath("$.message").value("Department not found with id: 999"))
+                .andExpect(jsonPath("$.errorCode").value("RECORD_NOT_FOUND"));
+    }
+    //
+
+}
