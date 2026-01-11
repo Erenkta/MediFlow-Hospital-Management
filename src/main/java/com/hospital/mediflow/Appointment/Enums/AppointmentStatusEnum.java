6647778d56e5025b8e40a84c91f8435408187153
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2026-01-11 14:02:28 +0300

    Add new constraint to appointment status

--- src/main/java/com/hospital/mediflow/Appointment/Enums/AppointmentStatusEnum.java
+++ src/main/java/com/hospital/mediflow/Appointment/Enums/AppointmentStatusEnum.java
@@ -2,6 +2,9 @@ package com.hospital.mediflow.Appointment.Enums;
 
 public enum AppointmentStatusEnum {
     PENDING,
+    CANCELLED,
+    NOT_ATTENDED,
+    ON_GOING,
     APPROVED,
     REJECTED,
     DONE,
