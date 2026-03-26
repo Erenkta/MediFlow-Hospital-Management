7f1f28f843dfc8eb99af2b84788a500c0acc5b20
Eren Nokta <109761393+Erenkta@users.noreply.github.com>
2026-03-26 18:37:20 +0300

    Invoice PDF scheduler implemented

--- src/main/java/com/hospital/mediflow/Billing/Repositories/BillingRepository.java
+++ src/main/java/com/hospital/mediflow/Billing/Repositories/BillingRepository.java
@@ -2,6 +2,7 @@ package com.hospital.mediflow.Billing.Repositories;
 
 import com.hospital.mediflow.Billing.Domain.Entity.Billing;
 import com.hospital.mediflow.Common.BaseRepository;
+import com.hospital.mediflow.Common.Dto.InvoicePdfProjection;
 import com.querydsl.core.types.Predicate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,8 +11,8 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BillingRepository extends BaseRepository<Billing,Long>, QuerydslPredicateExecutor<Billing> {
@@ -35,4 +36,23 @@ public interface BillingRepository extends BaseRepository<Billing,Long>, Queryds
     """
     )
     Billing findBillingByAppointment(@Param("appointment_id") Long appointmentId);
+
+    @Query(
+        """
+        Select 
+        b.id as id,
+        p.firstName as customerName,
+        p.lastName as customerLastName,
+        b.billingDate as billingDate,
+        a.appointmentDate as appointmentDate,
+        a.reason as appointmentDescription,
+        b.amount as amount
+        from Billing b
+        inner join Appointment a on b.appointment.id = a.id
+        inner join Patient p on b.patient.id = p.id
+        
+        where b.billingDate >= :start AND b.billingDate < :end
+        """
+    )
+    List<InvoicePdfProjection> findBillingsByDateRanged(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 }
--- src/main/java/com/hospital/mediflow/MediflowApplication.java
+++ src/main/java/com/hospital/mediflow/MediflowApplication.java
@@ -2,8 +2,10 @@ package com.hospital.mediflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MediflowApplication {
 
 	public static void main(String[] args) {
